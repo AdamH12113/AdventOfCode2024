11825d723a079327f3dfc5ceb85be50a67926b7d
Adam Haun <adam.haun@microsiliconinc.com>
2024-12-10 13:20:03 -0600

    Completed day 6. Added Vector class.

--- Day6.py
+++ Day6.py
@@ -1,4 +1,5 @@
 import re, sys, copy
+from Vector import Vector
 
 # Read the input
 try:
@@ -12,16 +13,57 @@ except Exception as e:
 	exit()
 
 # Process the input. It's a 2D grid containing a map of a facility with a guard (^) and obstacles (#)
-# along with empty space (.).
+# along with empty space (.). To make working with vectors easier, the start of the input will be the
+# maximum y coordinate.
 grid = [line for line in input_text.split('\n')]
 size = len(grid)
-obstacle_coords = []
-guard_coords = (-1, -1)
+obstacles = set()
+guard_coords = Vector(-1, -1)
 for x in range(size):
 	for y in range(size):
 		if grid[y][x] == '#':
-			obstacle_coords.append((x, y))
+			obstacles.add(Vector(x, (size - 1) - y))
 		elif grid[y][x] == '^':
-			guard_coords = (x, y)
+			guard_coords = Vector(x, (size - 1) - y)
 
-# Part 1: How many distinct positions will the guard visit before leaving the area?
+# Part 1: How many distinct positions will the guard visit before leaving the area? Let's do this
+# the naive way so we can see how part 2 will pull the rug out from under us.
+position = guard_coords
+direction = Vector(0, 1)
+visited = set()
+while position.in_range(0, size - 1, 0, size - 1):
+	visited.add(position)
+	while position + direction in obstacles:
+		direction = direction.rotate_cw()
+	position += direction
+print(f"Part 1: The guard will visit {len(visited)} distinct positions.")
+
+# Part 2: In how many places (aside from the starting location) could a new obstacle be added that
+# would cause the guard to move in a loop? Only obstacles added to a coordinate that was visited in
+# part 1 can make a difference, so we can cut down on the number of locations a bit. It's still slow,
+# but oh well.
+def guard_goes_in_loop(obstacles):
+	position = guard_coords
+	direction = Vector(0, 1)
+	visited_corners = set()
+	while True:
+		while position + direction not in obstacles:
+			position += direction
+			if not position.in_range(0, size - 1, 0, size - 1):
+				return False
+		if (position, direction) in visited_corners:
+			return True
+		else:
+			visited_corners.add((position, direction))
+			direction = direction.rotate_cw()
+
+obstruction_locs = 0
+for x in range(size):
+	print(x)
+	for y in range(size):
+		new_obstacle = Vector(x, y)
+		if new_obstacle not in obstacles and new_obstacle != guard_coords and new_obstacle in visited:
+			modified_obstacles = obstacles | set([new_obstacle])
+			if guard_goes_in_loop(modified_obstacles):
+				obstruction_locs += 1
+print(f"Part 2: The number of possible obstruction locations is {obstruction_locs}.")
---
+++ Vector.py
@@ -0,0 +1,63 @@
+# Vector classes for working with coordinates. These are implemented as immutable dataclasses
+# so that they can be hashed (stored in sets, etc.).
+from dataclasses import dataclass
+from typing_extensions import Self
+
+@dataclass(frozen = True)
+class Vector:
+	x: int
+	y: int
+	
+	def __add__(self, v: Self) -> Self:
+		return type(self)(self.x + v.x, self.y + v.y)
+
+	def __sub__(self, v: Self) -> Self:
+		return type(self)(self.x - v.x, self.y - v.y)
+	
+	def __neg__(self) -> Self:
+		return type(self)(-self.x, -self.y)
+	
+	def __mul__(self, c: int) -> Self:
+		return type(self)(c * self.x, c * self.y)
+	
+	def __rmul__(self, c: int) -> Self:
+		return self * c
+	
+	def __str__(self) -> str:
+		return f"({self.x},{self.y})"
+	
+	def in_range(self, xmin: int, xmax: int, ymin: int, ymax: int) -> bool:
+		return self.x >= xmin and self.x <= xmax and self.y >= ymin and self.y <= ymax
+	
+	def rotate_cw(self) -> Self:
+		return type(self)(self.y, -self.x)
+	
+	def rotate_ccw(self) -> Self:
+		return type(self)(-self.y, self.x)
+
+@dataclass(frozen = True)
+class Vector3:
+	x: int
+	y: int
+	z: int
+	
+	def __add__(self, v: Self) -> Self:
+		return type(self)(self.x + v.x, self.y + v.y, self.z + v.z)
+
+	def __sub__(self, v: Self) -> Self:
+		return type(self)(self.x - v.x, self.y - v.y, self.z - v.z)
+	
+	def __neg__(self) -> Self:
+		return type(self)(-self.x, -self.y, -self.z)
+	
+	def __mul__(self, c: int) -> Self:
+		return type(self)(c * self.x, c * self.y, c * self.z)
+	
+	def __rmul__(self, c: int) -> Self:
+		return self * c
+	
+	def __str__(self) -> str:
+		return f"({self.x},{self.y},{self.z})"
+
+	def in_range(self, xmin: int, xmax: int, ymin: int, ymax: int, zmin, zmax) -> bool:
+		return self.x >= xmin and self.x <= xmax and self.y >= ymin and self.y <= ymax and self.z >= zmin and self.z <= zmax
