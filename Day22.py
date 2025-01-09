6c6ad70f72d4051e57cb7fac228cc37677fbb382
Adam Haun <adam.haun@microsiliconinc.com>
2025-01-09 15:02:22 -0600

    Completed day 22.

--- Day22.py
+++ Day22.py
@@ -50,10 +50,36 @@ print(f"Part 1: The sum of the 2000th secrets is: {total}")
 
 # Part 2: The ones digits of the secret numbers are prices. We need to find a sequence of four
 # price *changes* that will trigger a buy on each list of prices such that we get the maximum
-# total price. Yeesh. Not even sure where to start with this one.
+# total price. Yeesh. Not even sure where to start with this one. One of my friends said it can
+# be brute-forced, so I guess I'll try that. I'll need to be at least a little efficient about it.
 price_lists = [[s % 10 for s in secrets] for secrets in secret_lists]
-derivatives = [[prices[n] - prices[n-1] for n in range(1, len(prices))] for prices in price_lists]
-print(derivatives[0])
+derivative_lists = [[prices[n] - prices[n-1] for n in range(1, len(prices))] for prices in price_lists]
+der_start_indices = []
+for derivatives in derivative_lists:
+	starts = {n: [] for n in range(-9, 9 + 1)}
+	for d in range(len(derivatives)):
+		starts[derivatives[d]].append(d)
+	der_start_indices.append(starts)
 
+def bananas_for_derivatives(d1, d2, d3, d4):
+	total = 0
+	for buyer in range(len(price_lists)):
+		prices = price_lists[buyer]
+		derivatives = derivative_lists[buyer]
+		starts = der_start_indices[buyer][d1]
+		for d in starts:
+			if d < len(derivatives) - 3 and derivatives[d+1] == d2 and derivatives[d+2] == d3 and derivatives[d+3] == d4:
+				total += prices[d+4]
+				break
+	return total
 
-
+max_bananas = 0
+for d1 in range(-9, 9 + 1):
+	for d2 in range(-9, 9 + 1):
+		#print(d1, d2)
+		for d3 in range(-9, 9 + 1):
+			for d4 in range(-9, 9 + 1):
+				total = bananas_for_derivatives(d1, d2, d3, d4)
+				if total > max_bananas:
+					max_bananas = total
+print(f"Part 2: The most maximum possible number of bananas is: {max_bananas}")
